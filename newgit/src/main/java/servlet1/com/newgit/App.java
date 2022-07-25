b251ac6bc3dddd919291efa3ac5203b1b65ec78a
siva <kpoorna.16524@gmail.com>
2022-07-25 13:06:28 +0530

    First commit

---
+++ newgit/src/main/java/servlet1/com/newgit/App.java
@@ -0,0 +1,13 @@
+package servlet1.com.newgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ newgit/src/main/java/servlet1/com/newgit/Demo.java
@@ -0,0 +1,11 @@
+package servlet1.com.newgit;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
